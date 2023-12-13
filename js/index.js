a05ce174237c75e98886f8c270a8395d7272185f
sironeko0295 <113327709+sironeko0295@users.noreply.github.com>
2023-12-13 23:15:19 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-$('.slider').slick({
-  autoplay: true, //自動的に動き出すか。初期値はfalse
-  autoplayspeed:3000, //次の画像に切り替わる速度　1000 = 1s
-  speed:1000, //スライドの動きの速さ。初期値は300
-  infinite:true, //無限ループするか
-  arrows:true, //左右の矢印あり
-})
+
